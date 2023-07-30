d4c07d72066e9c44450dc337c4ce5427e9778256
hi-here <71664582+hi-here@users.noreply.github.com>
2023-07-30 09:16:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".pre-btn",
+        nextArrow:".next-btn",
+
+    });
+
+});
+
